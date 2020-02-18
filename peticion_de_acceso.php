73724502450e1cf8f485bef40ed34b5118e984f4
frcandroid <frc13elche@gmail.com>
2020-02-18 23:46:22 +0100

    K

--- peticion_de_acceso.php
+++ peticion_de_acceso.php
@@ -3,11 +3,13 @@
 include 'base_datos.php';
 //en teoria nos hemos conectado con el usuario LIMITADO
 //comprobamos la validez de los datos introducidos
-
 if(isset($_POST['alta'])){
     $errores = validarDatos();
+    var_dump($errores);
+    ob_end_clean();
     header("Content-Type: application/json");
-    echo json_encode($errores);
+   // echo json_encode($errores);
+    die();
 }
 
 
@@ -105,11 +107,13 @@ function validarDatos(){//listo para pasar a MVC
         }
         else{//comprobamos que el email existe o no en la base de datos, tanto en solicitudes como en clientes
             $conexion = conectar();
-            $consulta = "SELECT s.EMAIL c.EMAIL FROM solicitudes s FULL JOIN clientes c WHERE EMAIL = :email";
-            $conexion->prepare($consulta);
-            $resultado = $conexion->execute([":email" => $email]);
+            $consulta = "SELECT EMAIL
+                         FROM solicitudes 
+                         WHERE EMAIL = :email";
+            $resultado = $conexion->prepare($consulta);
+            $emails= $resultado->execute([":email" => $email]);
             //si hay resultados, el email ya existe en la base de datos
-            if($resultado->rowCount() > 0){
+            if($emails === true){
                 $errores['email'] = "Esta dirección ya ha sido usada";
                 $valido = false;
             }
@@ -131,11 +135,11 @@ function validarDatos(){//listo para pasar a MVC
         }
         else{//comprobamos que el nick existe o no en la base de datos, tanto en solicitudes como en clientes
             $conexion = conectar();
-            $consulta = "SELECT s.NICK c.NICK FROM solicitudes s FULL JOIN clientes c WHERE NICK = :nick";
-            $conexion->prepare($consulta);
-            $resultado = $conexion->execute([":nick" => $nick]);
+            $consulta = "SELECT NICK FROM solicitudes WHERE NICK = :nick";
+            $resultado = $conexion->prepare($consulta);
+            $nicks = $resultado->execute([":nick" => $nick]);
             //si hay resultados, el nick ya existe en la base de datos
-            if($resultado->rowCount() > 0){
+            if($nicks === true){
                 $errores['nick'] = "Este nick ya esta en uso";
                 $valido = false;
             }
@@ -158,34 +162,36 @@ function validarDatos(){//listo para pasar a MVC
 
 
 ?>
+
 <!DOCTYPE html>
 <head>
-<meta charset="utf-8">
-  <script src="jquery.js"></script>
-  <script src="script.js"></script>
-<link rel='stylesheet' type='text/css' href='trabajodaw.css'>
-<link rel='stylesheet' type='text/css' href='bootstrap.css'>
+    <meta charset="utf-8">
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+    <link rel='stylesheet' type='text/css' href='trabajodaw.css'>
+    <link rel='stylesheet' type='text/css' href='bootstrap.css'>
 </head>
 <body>
 <h1>Formulario de solicitud de alta</h1>
-<form method='post' action=<?=$_SERVER['PHP_SELF']?>>
+<form id='form' method='post' action=<?=$_SERVER['PHP_SELF']?>>
     <label class="required col-md-2 offset-md-3 col-form-label" for='nombre'>Nombre </label>
-        <input type='text' name='nombre' id='nombre'>
-<br><br>
+    <input type='text' name='nombre' id='nombre'>
+    <br><br>
     <label class="required col-md-2 offset-md-3  col-form-label " for='cif'>CIF </label>
-        <input type='text' name='cif' id='cif'><br><br>
+    <input type='text' name='cif' id='cif'><br><br>
     <label class="required col-md-2  offset-md-3 col-form-label" for='razonsocial'>Razon Social </label>
-        <input type='text' name='razonsocial' id='razonsocial'><br><br>
+    <input type='text' name='razonsocial' id='razonsocial'><br><br>
     <label class="required col-md-2  offset-md-3 col-form-label" for='tlf'>Teléfono </label>
-        <input type='tel' name='tlf' id='tlf'><br><br>
+    <input type='tel' name='tlf' id='tlf'><br><br>
     <label class="required col-md-2  offset-md-3 col-form-label" for='email'>Correo electrónico </label>
-        <input type='email' name='email' id='email'><br><br>
+    <input type='email' name='email' id='email'><br><br>
     <label class="required col-md-2  offset-md-3 col-form-label" for='nick'>Nick (debe ser único) </label>
-        <input type='text' name='nick' id='nick'><br><br>
+    <input type='text' name='nick' id='nick'><br><br>
     <label class="required col-md-2  offset-md-3 col-form-label" for='pass'>Contraseña </label>
-        <input type='pass' name='pass' id='pass'><br><br>
-        <input type='submit' id='alta' data-user="alta" class='btn btn-default btn-info'>Solicitar alta</input>
+    <input type='pass' name='pass' id='pass'><br><br>
+    <input type='submit' id='alta' name="alta" data-user="alta" class='btn btn-default btn-info' value="Solicitar alta">
     <input type='hidden' name='usuario' id='usuario' value='LIMITADO'>
 </form>
 </body>
 <!-- esto se comunica primero con ajax -->
+
--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {//entra aqui cuando la página ha cargado con exito
-    
-    // getdetails será nuestra función para enviar la solicitud ajax    
-    let getdetails = function () {
-        let variables = $.ajax({
+    // getdetails será nuestra función para enviar la solicitud ajax
+    let getdetails = function (values) {
+        let peticion = $.ajax({
             url: "peticion_de_acceso.php",
-            dataType: "json",
             type: "post",
-            data: $("#alta").serialize()
+            data: values,
+            contentType: "application/json; charset=utf-8"
         })
+        return peticion;
     }
 
     ;
 
     // Al hacer click sobre cualquier elemento que tenga el atributo data-user.....
-    $('[data-user]').click(function(e) {
+    $('#alta').click(function(e) {
+        console.log($('form'));
+        let values = $('form').serialize();
+        console.log(values);
 
         $("#textFail").remove();//eliminamos el elemento de fallo por si ahora sale bien
         e.preventDefault();
         // this hace referencia al elemento que ha lanzado el evento click
         // con el método .data('user') obtenemos el valor del atributo data-user de dicho elemento y lo pasamos a la función getdetails definida anteriormente
-        getdetails()
+        getdetails(values)
         
         .done(function(response) {//entra aqui cuando sale bien la llamada
-            
+            console.log(response);
             //done() es ejecutada cuándo se recibe la respuesta del servidor. response es el objeto JSON recibido
             if (response.success) {
                 //esto es si la llamada se ha hecho correctamente, ahora comprobamos la validez de los datos
-                console.log(response);
                 //nombre
 
                 var output = "<h1>" + response.data.message + "</h1>";
@@ -71,4 +73,4 @@ $(document).ready(function() {//entra aqui cuando la página ha cargado con exit
         
     });
     
-});
+});*/
